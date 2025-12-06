9b1a37b8b1b21a12d6b54fba604227bd9b03e6d6
Muhammad Hunain <hunainamir76@gmail.com>
2025-12-07 00:57:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var username;
+
+var myName = "Hunain Amir";  
+
+var message;
+
+
+message = "Hello World";
+
+alert(message);
+
+var studentName = "Hunain Amir";
+var age = "18 years old";
+var course = "Web Development";
+
+alert("Student Name: " + studentName);
+alert("Age: " + age);
+alert("Course: " + course);
+
+
+var pattern = "HUNAIN\nHUNA\nHUN\nHU\nH";
+alert(pattern);
+
+
+var email = "hunainamir76@gmail.com"; 
+
+alert("My email address is " + email);
+
+
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the book: " + book);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+alert(design);
+
+document.write(design);
+
+
